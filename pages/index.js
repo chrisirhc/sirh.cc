2e277d8d5fbde1032aad9017bd3ca93debbedb4b
Chris Chua <chris.sirhc@gmail.com>
2024-02-24 20:07:11 +0800

    clean up styles

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Chris Chua</title>
       </Head>
-      <div className="container">
-        <div className="jumbotron" id="me">
-          <h1>Chris Chua</h1>
-          <p>Visualizing with code.</p>
-          <div className="row">
-            <div className="col-sm-4 text-center">
-              <p>
-                <a href="http://blog.sirh.cc/">Blog</a>
-              </p>
-            </div>
-            <div className="col-sm-4 text-center">
-              <p>
-                <a href="http://github.com/chrisirhc/">Code</a>
-              </p>
-            </div>
-            <div className="col-sm-4 text-center">
-              <p>
-                <a href="http://about.me/chrisirhc">about.me</a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="text-center" id="svg-container"></div>
-      </div>
-    </div>
+      <main>
+        <h1>Chris Chua</h1>
+        <p>Software engineer.</p>
+        <ul>
+          <li>
+            <a href="http://blog.sirh.cc/">Blog</a>
+          </li>
+          <li>
+            <a href="http://github.com/chrisirhc/">Github</a>
+          </li>
+          {/* <a href="http://about.me/chrisirhc">about.me</a> */}
+        </ul>
+      </main>
+    </>
   );
 }
